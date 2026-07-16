0ee75be0d805ed2ccb6f361557915580bc4d4eba
Ayush Shivhare <ayushshivhare351@gmail.com>
2026-07-16 23:20:57 +0530

    Sprint 6: Worker fetches jobs from PostgreSQL

--- apps/api/src/worker.ts
+++ apps/api/src/worker.ts
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import { redis } from "./lib/redis";
 import { prisma } from "./lib/prisma";
+import { sleep } from "./utils/sleep";
 
 async function startWorker() {
     console.log("👷 Worker started...");
@@ -8,7 +9,7 @@ async function startWorker() {
     while (true) {
         const result = await redis.brPop("queue:default", 0);
         const jobId = result.element;
-        
+
         console.log("Received Job:", jobId);
 
         const job = await prisma.job.findUnique({
@@ -16,6 +17,30 @@ async function startWorker() {
                 id: jobId,
             },
         });
+
+        await prisma.job.update({
+            where: {
+                id: jobId,
+            },
+            data: {
+                status: "PROCESSING",
+            },
+        });
+
+        console.log("Job is processing...");
+
+        await sleep(3000);
+
+        await prisma.job.update({
+            where: {
+                id: jobId,
+            },
+            data: {
+                status: "COMPLETED",
+            },
+        });
+
+        console.log("Completed!");
 
         console.log(job);
     }
